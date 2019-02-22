6f02b9201534f2b32e19f20dba6e3951fbfddd0d
Nicola Olivetto <nicola.olivetto@triboo.it>
2019-02-22 15:35:56 +0100

    added resize of local file

--- server.js
+++ server.js
@@ -57,7 +57,44 @@ const serveResized = (event, context) => {
     }
 }
 
+const serveLocalResized = (event, context) => {
+    const baseUrl = './media';
+    const requestUrl = Url.parse(event.originalUrl);
+    const pathname = requestUrl.pathname.replace('/resizedimage/','/');
+    const extension = path.extname(pathname);
+    const name = event.query.name ? (event.query.name + extension) : path.basename(pathname);
+    const download = Boolean(event.query.download) || false
+    const format = event.query.format || 'jpg'
+
+    const headers = [];
+    headers['Cache-Control'] = 'public, max-age=2592000';
+    headers['Expires'] = new Date(Date.now() + 2592000000).toUTCString();
+
+    if (download) {
+        headers['content-disposition'] = `attachment; filename="${name}"`;
+        context.setHeader('Content-Disposition', headers['content-disposition']);
+    } else {
+        headers['content-disposition'] = `inline; filename="${name}"`;
+    }
+
+    if ((event.query.w || event.query.h) && (extension === '.png' || extension === '.jpg' || extension === '.jpeg')) {
+        const width = event.query.w ? Number(event.query.w) : null;
+        const height = event.query.h ? Number(event.query.h) : null;
+        const img = baseUrl + pathname;
+        context.type(`image/${format}`);
+
+        buffer(img, format, width, height).pipe(context);
+
+    } else {
+        context
+            .status(200)
+            .type('image/jpg')
+            .send(body);
+    }
+}
+
 server.get('/', serveResized);
+server.get('/resizedimage/*', serveLocalResized);
 
 server.listen(5000, () => {
     console.log('Server started!');
